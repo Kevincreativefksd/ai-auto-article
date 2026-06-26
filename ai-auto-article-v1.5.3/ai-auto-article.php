0470263ec26ae1194efbeb0c6c950e6f8742bcd3
Kevincreativefksd <fakhsyadmuhammad@gmail.com>
2026-06-26 23:59:37 +0700

    update

---
+++ ai-auto-article-v1.5.3/ai-auto-article.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: AI Auto Article Gemini Pro
+Description: Generate artikel otomatis dengan Brand Context, Informasi Tambahan Opsional, dan Internal Link Beranda.
+Version: 1.5.2
+Author: Muhammad Arfakhsyad
+*/
